6979c2552aac57252b064012c236873fc0dd2436
Julian Barbosa <barbosabjf@gmail.com>
2023-12-11 18:14:38 -0300

    add sign and verify usage

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("bom dia")
+import { DAY_IN_MILLISECONDS, SECRET } from "./utils";
+import { sign } from "./jwt/sign";
+import { verify } from "./jwt/verify";
+
+const token = sign({
+	exp: Date.now() + DAY_IN_MILLISECONDS,
+	data: {
+		sub: "@julian.barbosa",
+		roles: ["admin"],
+	},
+	secret: SECRET,
+});
+
+const decoded = verify({
+	token,
+	// to validate, you can just switch the secret to another data
+	// or change any data from the token above
+	secret: SECRET,
+});
+
+console.log("decoded", decoded);
